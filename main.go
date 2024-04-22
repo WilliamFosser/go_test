6a9e99e3da0f91edf0782559eea56854c82c126d
William Fosser <fosserwilliam@gmail.com>
2024-04-22 20:36:10 +0200

    First working program

--- main.go
+++ main.go
@@ -18,21 +18,18 @@ func main() {
 		return
 	}
 
-	/*
-			err = d.SetBitrate(125000)
-			if err != nil {
-				fmt.Println("Failed to set bitrate:", err)
-				return
-			}
+	err = d.SetBitrate(500000)
+	if err != nil {
+		fmt.Println("Failed to set bitrate:", err)
+		return
+	}
 
+	err = d.SetUp()
+	if err != nil {
+		fmt.Println("Failed to set up CAN device:", err)
+		return
+	}
 
-		err = d.SetUp()
-		if err != nil {
-			fmt.Println("Failed to set up CAN device:", err)
-			return
-		}
-
-	*/
 	defer d.SetDown()
 
 	// Receive CAN frames
@@ -47,4 +44,5 @@ func main() {
 		frame := recv.Frame()
 		fmt.Println(frame.String())
 	}
+
 }
